7101bd18510b4e67c054fb00244640634446871a
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2022-05-05 18:44:35 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Hindi-Urdu Language Parser
 ## We utilize Lark (Python based) to build a basic human language parser
+### Our parser does the following:
+* Follows Hindi and Urdu grammar rules
+* Follows Hindi and Urdu sentence structure
+* Contains several sample sentences in the corpus for testing
+
+Note: While the vocabulary of Urdu and Hindi are quite different, on the conversational level, they are quite similar and can be interchangeably spoken, hence we wanted to create 
+this parser. Below we will explore some more rules of Hindi grammar and it's major differences
+from English grammar.
+
+## General Rules of Hindi Grammar
+1. Grammar in Hindi follows the S-O-V order as opposed to S-V-O order used in English
+2. Feminine and Masculine genders exist for all words
+3. Multitude of ways to address other people (pronouns) that depend on the level of respect & formality given to them
+4. Verb conjugation
+    4a. For the purposes of this project, we have decided to not handle
+    verb conjugation with the parser, as it can introduce a lot of complexity. 
 
 Usage 
 ```
 python3 parser.py
 ```
+
--- parser.py
+++ parser.py
@@ -16,13 +16,13 @@ det: "ek" | "vo" | "ye" | "meri" | "us"
 
 conj: "aur" | "lekin" | "ya" | "kyunki"
 
-n: "aadmi" | "aurat" | "dhoorbeen" | "jhaad" | "chali" | "raste" | "tasveer" | "ladki" | "beti" | "daakter" | "ladka" | "chai" | "taare"
+n: "aadmi" | "aurat" | "dhoorbeen" | "jhaad" | "chali" | "raste" | "tasveer" | "ladki" | "beti" | "daakter" | "ladka" | "chai" | "taare" | "ghar" | "shaadi"
 
-v: "dekhraha" | "chalraha" | "karni" | "kheencho" | "khadi" | "soti" | "shaadi" | "bhanaraha"
+v: "dekhrahi" | "chalraha" | "chalrahi" | "karni" | "kheencho" | "khadi" | "soyi" | "shaadi" | "bhanaraha"
 
-prep: "ke saath" | "ke uppar" | "aise" | "par" | "pe" | "ke paas" | "jaise" | "se" | "ke baad" | "ke piche"  
+prep: "ke saath" | "ke uppar" | "aise" | "par" | "pe" | "ke paas" | "jaise" | "se" | "ke baad" | "ke piche" | "ko" | "mein"
 
-adj: "badaa" | "chota" | "budda" | "jawaan" | "naraaz" | "lambi" | 
+adj: "badaa" | "choti" | "budda" | "jawaan" | "naraaz" | "lambi" | 
 
 adv: "kaise" | "hai" | "kareygi"
 
@@ -34,12 +34,26 @@ corpus = """
 ye ladki khadi hai
 vo ladki jhaad ke piche khadi hai
 ke piche khadi hai jhaad ladki vo 
-ye naraaz ladki khadi hai aur aadmi chalraha hai lekin aurat soti hai
 vo budda aadmi chalraha hai
 meri beti daakter se shaadi kareygi
 vo ladka chai bhanaraha hai
-vo ladki dhoorbeen se taare dekhraha hai
+vo ladki dhoorbeen se taare dekhrahi hai
 meri beti us aurat ke saath tasveer kheencho
+vo lambi jhaad ke paas ek aurat raste ko dekhrahi hai
+vo ghar mein shaadi chalrahi hai lekin ghar ke uppar choti beti soyi hai
+"""
+"""
+Translated sentences:
+This girl stands
+That girl stands behind the tree
+Behind stands the tree girl that
+That old man is walking
+My daughter will marry a doctor
+That boy is making tea
+That girl is looking at the stars through a telescope
+Take a picture of my daughter with that woman
+A woman near a tall tree looks at the street 
+There is a wedding going on in that house but a girl sleeps above the house.
 """
 for sent in corpus[1:].split('\n'):
   print("======\nsentence=",sent)
